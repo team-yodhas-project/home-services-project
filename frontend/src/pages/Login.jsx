3646eccd43876557e00b630fff303537ad06f02a
NaralaVyshnavi <naralavyshnavi25@gmail.com>
2026-05-18 22:49:22 +0530

    auth error solved

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,8 +2,8 @@ import "../styles/auth.css";
 import { useFormik } from "formik";
 import { useNavigate } from "react-router-dom";
 import { useState, useRef, useEffect } from "react";
-import { useLoginUserMutation } from "../features/auth/authAPI";
-import { useGetProfileQuery } from "../features/auth/authAPI";
+import { useLoginUserMutation } from "../features/auth/authAPI.js";
+import { useGetProfileQuery } from "../features/auth/authAPI.js";
 
 function Login() {
   const navigate = useNavigate();
